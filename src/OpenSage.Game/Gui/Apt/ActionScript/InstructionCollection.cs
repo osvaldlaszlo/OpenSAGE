5e451ae5da3b059be63b8c2a4f71a0e56f46199e
Stephan Vedder <stephan.vedder@gmail.com>
2019-02-09 22:11:48 +0100

    More work on buttons

--- src/OpenSage.Game/Gui/Apt/ActionScript/InstructionCollection.cs
+++ src/OpenSage.Game/Gui/Apt/ActionScript/InstructionCollection.cs
@@ -57,7 +57,7 @@ namespace OpenSage.Gui.Apt.ActionScript
                 }
 
                 InstructionBase instruction = null;
-                List<Value> parameters = new List<Value>();
+                var parameters = new List<Value>();
 
                 switch (type)
                 {
@@ -192,6 +192,9 @@ namespace OpenSage.Gui.Apt.ActionScript
                         break;
                     case InstructionType.BitwiseXOr:
                         instruction = new BitwiseXOr();
+                        break;
+                    case InstructionType.ShiftRight2:
+                        instruction = new ShiftRight2();
                         break;
                     case InstructionType.Greater:
                         instruction = new Greater();
